8d82433634c2908715f5a1912dc08b1e7ccbd93e
Bora Yüret <byuret@fonetyazilim.com>
2021-10-16 17:30:02 +0300

    Employee entity ve repository oluşturuldu. application.properties dosyasına db ayarları eklendi.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+insert into employee(first_name, last_name, email) values ("Aziz", "Sancar","aziz@sancar.com");
+insert into employee(first_name, last_name, email) values ("Uğur", "Coşkun","ugur@coskun.com");
